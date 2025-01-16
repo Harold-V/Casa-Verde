08705030e5f249b550b5e1b4b04aee86db6a71d5
Harold Velasquez <hvelasquez@unicauca.edu.co>
2025-01-16 15:20:06 -0500

    Configuracion Hecha

---
+++ Backend/src/main/java/com/casaverde/backend/capaAccesoADatos/repositories/ConfiguracionRepository.java
@@ -0,0 +1,15 @@
+package com.casaverde.backend.capaAccesoADatos.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.casaverde.backend.capaAccesoADatos.models.ConfiguracionEntity;
+
+@Repository
+public interface ConfiguracionRepository extends CrudRepository<ConfiguracionEntity, String> {
+    @Query("SELECT p.pedID FROM PedidoEntity p")
+    List<Long> findAllProductIds();
+}
