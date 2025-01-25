c7b3d7ed15a0349239bb68655ade6f757bbce1a9
Harold Velasquez <hvelasquez@unicauca.edu.co>
2025-01-24 19:22:40 -0500

    Angular Standlone

--- Frontend/src/app/Home/home.component.ts
+++ Frontend/src/app/Home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  homeConfiguracion() {
-    console.log('Configuración');
-  }
+  homeConfiguracion() {}
 }
