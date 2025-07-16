da3638ec423a84e755641742e217486545364ecc
Harold Velasquez <hvelasquez@unicauca.edu.co>
2025-07-16 11:54:19 -0500

    falta main.js

--- Frontend/src/app/Configuracion/dataAccess/ConfiguracionEntity.ts
+++
@@ -1,9 +0,0 @@
-export class Configuracion {
-  configClave: string;
-  configValor: string;
-
-  constructor(configClave: string, configValor: string) {
-    this.configClave = configClave;
-    this.configValor = configValor;
-  }
-}
---
+++ frontend/electron/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // En desarrollo:
+  win.loadURL("http://localhost:5173");
+
+  // En producción, cuando uses build:
+  // win.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
